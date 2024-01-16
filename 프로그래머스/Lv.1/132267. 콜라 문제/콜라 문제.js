08d80f3f98876fbf01d43f2b173b827b974c8b72
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-01-16 09:34:18 +0900

    [level Lv.1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/Lv.1/132267. 콜라 문제/콜라 문제.js
@@ -17,3 +17,22 @@ function solution(a, b, n) {
 
     return answer;
 }
+/*
+1) function solution(a, b, n): 콜라를 받기 위한 함수를 정의하며, 매개변수로는 빈 병을 교환하는 규칙을 나타내는 a, 교환 시 받는 콜라 병 수를 나타내는 b, 그리고 상빈이가 처음에 가지고 있는 빈 병의 개수를 나타내는 n이 주어진다.
+
+2) var answer = 0;: 최종적으로 상빈이가 받을 수 있는 콜라 병의 수를 저장하는 변수 answer를 초기화한다.
+
+3) var emptyBottles = n;: 초기에 상빈이가 가지고 있는 빈 병의 수를 emptyBottles 변수에 저장한다.
+
+4) while (emptyBottles >= a) {: 빈 병이 a보다 많을 때까지 계속해서 루프를 실행한다.
+
+5) var colaFromExchanges = Math.floor(emptyBottles / a) * b;: 현재 가지고 있는 빈 병으로 교환 가능한 콜라의 수를 계산한다.
+
+6) emptyBottles = colaFromExchanges + (emptyBottles % a);: 콜라를 받고 남은 빈 병의 수를 갱신한다.
+
+7) answer += colaFromExchanges;: 받은 콜라 수를 정답에 더한다.
+
+8) return answer;: 최종적으로 상빈이가 받을 수 있는 콜라 병의 수를 반환한다.
+
+9) console.log(solution(2, 1, 20));: 예제를 이용하여 함수를 호출하고 결과를 출력한다.
+*/
