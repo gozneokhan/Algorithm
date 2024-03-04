8080e25a30b3ea6d247fdfd924f747b97dcc8c5c
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-04 19:06:01 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -2,3 +2,9 @@ function solution(num1, num2) {
     var answer = ~~(num1 / num2)
     return answer;
 }
+
+// function solution(num1, num2) {
+//   return parseInt(num1 / num2);
+// }
+
+// const solution = (num1, num2) => Math.floor(num1 / num2)
