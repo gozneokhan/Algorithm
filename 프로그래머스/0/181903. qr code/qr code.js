12f18fd063d88c48988582e762b9d93b72e978b3
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-04 15:54:36 +0900

    [level 0] Title: qr code, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.js
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -10,3 +10,7 @@ function solution(q, r, code) {
 
 // 문자열 code의 각 인덱스를 q로 나눈 나머지가 r인 경우 찾기 
 // 인덱스의 문자를 순서대로 추출해서 새로운 문자열 만들기 
+
+// function solution(q, r, code) {
+//   return [...code].filter((_, i) => i % q === r).join('');
+// }
