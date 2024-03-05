1cef305992c38486f844503ca0f1d3e2b2b6560c
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-05 10:12:59 +0900

    [level 0] Title: 짝수의 합, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -2,3 +2,18 @@ function solution(n) {
     var answer = 0;
     return Array(n).fill().map((_, i) => i + 1).filter(v => v % 2 === 0).reduce((a, c) => a + c, 0);
 }
+
+// function solution(n) {
+//     var half = Math.floor(n/2);
+//     return half*(half+1);
+// }
+
+// function solution(n) {
+//     let answer = 0
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 === 0) {
+//             answer += i            
+//         }
+//     }
+//     return answer
+// }
