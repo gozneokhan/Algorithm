5e35e5b918256d86431a1d5f3f9b4bb775821459
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-05 10:30:48 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,3 +1,20 @@
 function solution(numbers) {
     return numbers.reduce((a, c) => (a + c) ,0) / numbers.length
 }
+
+// function solution(numbers) {
+//     var answer = 0;
+//     for(i of numbers) {
+//         answer += i
+//     }
+//     return answer / numbers.length;
+// }
+
+// function solution(numbers) {
+//     var sum = 0
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += numbers[i]
+//     }
+//     var answer = sum / numbers.length;
+//     return answer;
+// }
