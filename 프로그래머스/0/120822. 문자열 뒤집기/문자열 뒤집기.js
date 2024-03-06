f4a19546b43c8d2e338ee2900fdb3355928ab5cf
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-06 15:13:25 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,4 +1,12 @@
 function solution(my_string) {
-    return my_string.split('').reverse().join('');
+    var answer = [...my_string].reverse().join("");
+    return answer;
 }
 
+// function solution(my_string) {
+//     return my_string.split('').reverse().join('');
+// }
+
+// function solution(my_string) {
+//     return Array.from(my_string).reverse().join('');
+// }
