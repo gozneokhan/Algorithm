459acd67f502106e4f081ff4071f54d2e34bfb83
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-06 13:16:20 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -2,3 +2,17 @@ function solution(numbers) {
    return numbers.map(v => v * 2);
 }
 // map()은 단순히 어떤 벨류에 연산을 하는 것보단 특정한 값을 뽑아낼 때 주로 사용함
+
+// const solution = (numbers) => numbers.map((number) => number * 2)
+
+// function solution(numbers) {
+//     return numbers.reduce((a, b) => [...a, b * 2], []);
+// }
+
+// function solution(numbers) {
+//     var answer = [];
+//     for(var i=0; i<numbers.length; i++){
+//         answer[i] = numbers[i] * 2;
+//     }
+//     return answer;
+// }
