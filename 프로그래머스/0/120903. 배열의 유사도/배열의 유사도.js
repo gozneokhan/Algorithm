2d1770bec94072903a38bf8e215d090d8fd51486
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-06 17:43:38 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,4 +1,20 @@
 function solution(s1, s2) {
-    var answer = 0;
-    return answer;
+   return s1.length + s2.length - new Set([...s1, ...s2]).size
 }
+
+// function solution(s1, s2) {
+//     var answer = 0;
+//     return s1.filter(v => s2.includes(v)).length;
+// }
+
+// function solution(s1, s2) {
+//     var answer = 0;
+    
+//     for (var i = 0; i < s1.length; i++) {
+//         // 현재 s1 요소가 s2 배열에도 존재하는지 확인
+//         if (s2.includes(s1[i])) {
+//             answer++;
+//         }
+//     }
+//     return answer;
+// }
