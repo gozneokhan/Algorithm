720c6c954ddaa6a91e909ab745c6e1dc91a0d583
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-06 15:43:27 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -2,3 +2,12 @@ function solution(my_string, letter) {
     const answer = my_string.split(letter).join('')
     return answer;
 }
+
+// function solution(my_string, letter) {
+//     return my_string.replaceAll(letter, "");
+// }
+
+// function solution(my_string, letter) {
+//     let reg = new RegExp(letter, 'g');
+//     return my_string.replace(reg, '');
+// }
