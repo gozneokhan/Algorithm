ae27534372837acdc05fc71ab94ff9fdef5e99a2
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-07 14:53:02 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -2,3 +2,23 @@ function solution(age) {
     let chr = 'abcdefghij'
    return Array.from(age.toString()).map(v => chr[v]).join('')
 }
+
+// function solution(age) {
+//     let chr = 'abcdefghij'
+//    return Array.from(String(age)).map(v => chr[v]).join('')
+// }
+
+// function solution(age) {
+//     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+//     var answer = "";
+//     age = age.toString();
+
+//     for(let i=0; i<age.length; i++){
+//         answer += alphabet[age[i]];
+//     }
+//     return answer;
+// }
+
+// function solution(age) {
+//   return age.toString().replace(/./g, (x) => "abcdefghij"[x]);
+// }
