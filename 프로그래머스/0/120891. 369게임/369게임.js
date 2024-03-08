79f9591af42d7751f85bae81851006d65868866b
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-08 22:40:51 +0900

    [level 0] Title: 369게임, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,13 +1,13 @@
-// function solution(order) {
-//     let value = order.toString().match(/[369]/g) ?? [];
-//     return value.length;
-// }
+function solution(order) {
+    let value = order.toString().match(/[369]/g) ?? [];
+    return value.length;
+}
 
 // function solution(order) {
 //    return order.toString().split('').filter(v => v === '3' || v === '6' || v === '9' ? true : false).length
 // }
 
-function solution(order) {
-    const s = new Set('369')
-   return order.toString().split('').filter(v =>s.has(v)  ? true : false).length
-}
+// function solution(order) {
+//     const s = new Set('369')
+//    return order.toString().split('').filter(v =>s.has(v)  ? true : false).length
+// }
