661a6d8ad408db7ce93832f26f741500400037a6
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-08 23:16:26 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,3 +1,11 @@
 function solution(my_string) {
     return [...new Set(my_string)].join('');
 }
+
+// let solution = s => [...s].filter((c,i) => s.indexOf(c) == i).join('')
+
+// function solution(my_string) {
+//     return my_string.split('').filter((value, index, self) => {
+//         return self.indexOf(value) === index;
+//     }).join('');
+// }
