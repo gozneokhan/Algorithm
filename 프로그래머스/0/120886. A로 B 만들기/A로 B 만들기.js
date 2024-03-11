5e5a4f78112acb93dceb279988de84fb16dfe147
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-11 20:40:20 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,3 +1,7 @@
 function solution(before, after) {
     return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
 }
+
+// function solution(before, after) {
+//     return [...before].sort().join('') === [...after].sort().join('') ? 1 : 0;
+// }
