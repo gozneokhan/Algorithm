94156da92b6913c8c87cdb46ac9044019dcec59b
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-12 21:31:08 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -5,3 +5,9 @@ function solution(s) {
 // function solution(s) {
 //    return [...s].filter(c => s.match(new RegExp(c, 'g')).length === 1).sort().join('');
 // }
+
+// function solution(s) {
+//     let res = [];
+//     for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
+//     return res.sort().join('');
+// }
