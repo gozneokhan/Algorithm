20964a096c529adf4f1a2ed49ff7c53f2dee8068
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-13 21:40:28 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -2,3 +2,31 @@ function solution(numbers) {
     return +numbers.replaceAll('one', '1').replaceAll('two', '2').replaceAll('three', '3').replaceAll('four', '4').replaceAll('five', '5').replaceAll('six', '6').replaceAll('seven', '7').replaceAll('eight', '8').replaceAll('nine', '9').replaceAll('zero', '0');
 }
 
+// function solution(numbers) {
+//     const number = ["zero","one","two","three", "four", "five", "six", "seven", "eight", "nine"]
+//     for(let i = 0 ; i<number.length; i++){
+//         numbers = numbers.split(number[i]).join(i)
+//     }
+//     return +numbers
+// }
+
+// let solution = n => Number(['zero','one','two','three','four','five','six','seven','eight','nine'].reduce((t,s,i) => t.replaceAll(s,i),n))
+
+// function solution(numbers) {
+//     const obj = {
+//         zero: 0, 
+//         one: 1, 
+//         two: 2, 
+//         three: 3, 
+//         four: 4,
+//         five: 5, 
+//         six: 6,
+//         seven: 7,
+//         eight: 8, 
+//         nine: 9,
+//     };
+
+//     return +numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+//         return obj[v];
+//     })
+// }
