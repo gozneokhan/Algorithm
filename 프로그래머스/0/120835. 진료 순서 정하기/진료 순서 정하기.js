f91318807c3788bafd49f33b41528070fa20660c
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-13 21:02:10 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,4 @@
 function solution(emergency) {
-    let emergency_order = emergency.slice().sort((a, b) => b - a);
+    let emergency_order = [...emergency].sort((a, b) => b - a);
     return emergency.map(v => emergency_order.indexOf(v) + 1);
 }
