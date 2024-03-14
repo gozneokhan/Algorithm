950e223b656ca1d3bd74038c29c959cacb47a37b
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-14 22:13:28 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,3 +1,24 @@
 function solution(A, B) {
     return (B + B).indexOf(A);
 }
+
+// let solution=(a,b)=>(b+b).indexOf(a)
+
+// function solution(A, B) {
+//     if (A===B) return 0;
+//     for (let i = 0; i < A.length; i++) {
+//         A = A.slice(-1) + A.slice(0,-1);
+//         if (A === B) return i+1;
+//     }
+//     return -1;
+// }
+
+// function solution(A, B) {
+//   const pushRight = str => [str[str.length - 1], ...str.slice(0, str.length - 1)].join('');
+//   for (let i = 0; i <= A.length; i++) {
+//     if (A === B) return i;
+//     A = pushRight(A);
+//   }
+
+//   return -1;
+// }
