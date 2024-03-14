74d95770f4f145eaa7e78494defc8e4b6dbe0c5c
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-14 21:49:55 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -2,3 +2,7 @@ function solution(spell, dic) {
     let answer = 0;
     return dic.some((v) => [...v].sort().toString() === [...spell].sort().toString()) ? 1 : 2;
 }
+
+// function solution(spell, dic) {
+//     return dic.filter(v=>spell.every(c=>v.includes(c))).length ? 1 : 2;
+// }
