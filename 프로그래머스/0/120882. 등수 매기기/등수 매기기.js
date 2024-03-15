d3863d7e3b7f7c087cc4f2de0af67454376c8026
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-15 11:30:56 +0900

    [level 0] Title: 등수 매기기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -3,3 +3,13 @@ function solution(score) {
     let arrangedArr = sum.slice().sort((a, b) => b -a);
    return sum.map((v) => arrangedArr.indexOf(v)+1);
 }
+
+// function solution(score) {
+//   return score.map((el) => {
+//     return (
+//       score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+//     );
+//   });
+// }
+
+// let solution=s=>s.map(([a,b])=>(a+b)/2).map((n,_,r)=>[...r].sort((a, b)=>b-a).indexOf(n)+1)
