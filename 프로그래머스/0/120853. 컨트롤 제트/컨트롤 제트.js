ef9e6e67bb124ee1cadef6cb3cf34ab861163a2d
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-15 11:10:26 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,7 +1,6 @@
 function solution(s) {
     s = s.split(' ');
     let result = [];
-    
     for (let i of s) {
         if (i === 'Z') {
             result.pop()
@@ -9,6 +8,22 @@ function solution(s) {
             result.push(+i)
         }
     }
-    
     return result.reduce((a, c) => a + c, 0);
 }
+
+// function solution(s) {
+//     const stack = []
+//     s.split(' ').forEach((target) => {
+//         if(target === 'Z') stack.pop();
+//         else stack.push(+target)
+//     })
+//     return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
+// }
+
+// function solution(s) {
+//     let arr = s.split(" ");
+//     while ( arr.indexOf('Z') > -1) {
+//         arr.splice( arr.indexOf('Z')-1, 2);
+//     }
+//     return arr.reduce((a,b) => parseInt(a) + parseInt(b),0)
+// }
