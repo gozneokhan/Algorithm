e838ef1a41b46dabc67bb957b6cb087cba03fc0d
SEWOONG KIM <154749631+gozneokhan@users.noreply.github.com>
2024-03-18 21:54:51 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -8,3 +8,13 @@ function solution(n) {
   }
     return answer;
 }
+
+// function solution(n) {
+//     let arr = [];
+//     for (let i = 0; i < 1000; i++) {
+//         if (i % 3 != 0 && !i.toString().split('').includes('3')) {
+//             arr.push(i)
+//         }
+//     }
+//     return arr[n-1];
+// }
